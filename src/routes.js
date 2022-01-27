8d873c3446e4918992c9ca1c980638288216c386
Cagla <caglaeyubog@gmail.com>
2022-01-27 11:58:42 +0300

    About and contact pages created/ routes update

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,16 @@ import Detail from "./components/Detail"
 import Login from "./components/Login"
 import Profile from "./components/Profile"
 import SortFilter from "./components/SortFilter"
-
+import About from "./components/About"
+import Contact from "./components/Contact"
 
 export const routes = [
     {name: "Home", pathname: "/", element: Home, isNav:false},
     {name: "Movies", pathname: "/sort-filter", element: SortFilter, isNav:false},
     {name: "Movies", pathname: "/sort-filter/:type", element: SortFilter, isNav:false},
-    {name: "Profile", pathname: "/profile", element: Profile, isNav:true},
+    {name: "Profile", pathname: "/profile", element: Profile, isNav:false},
+    {name: "About", pathname: "/about", element: About, isNav:true},
+    {name: "Contact", pathname: "/contact", element: Contact, isNav:true},
     {name: "Login", pathname: "/login", element: Login, isNav:true},
     {name: "Detail", pathname: "/movies/:movieId", element: Detail, isNav:false},
 ]
