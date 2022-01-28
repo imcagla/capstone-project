e0e0ebe50905c6ccc2ff37c0e5c9da19ca4c7697
Cagla <caglaeyubog@gmail.com>
2022-01-28 16:55:28 +0300

    search url

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,15 +3,22 @@ import { searchHandler } from '../reduxStore/searchHandle';
 import { useDispatch } from 'react-redux';
 import { Input } from '../styledComponents/SearchComponents';
 import { useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 function Search() {
   const dispatch = useDispatch()
-  const {theme} = useSelector(state => state)
+  const navigate = useNavigate()
+  const {theme,search} = useSelector(state => state)
   const themeName = theme ? "light" : "dark"
 
+
+
   return <div className='container my-3'>
-    <Input theme={themeName} type="text" placeholder='search' onChange={(e) => dispatch(searchHandler(e.target.value))}/>
+    <Input theme={themeName} type="text" placeholder='search' defaultValue={search} onChange={(e) => {
+      dispatch(searchHandler(e.target.value))
+      navigate(`/search?q=${e.target.value}`)
+    }}/>
   </div>
 }
 
